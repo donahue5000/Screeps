cea36eb5a218d14e8712ece29e22638391e8a557
Brian Donahue <donahue5000@gmail.com>
2019-02-10 18:19:03 -0800

    clean

--- Spawn2.js
+++ Spawn2.js
@@ -5,7 +5,8 @@
 var Spawn2 = {
     run: function(spawn){
 
-        var towers = Game.spawns[spawn].room.find(FIND_STRUCTURES, {filter: (structure) => structure.structureType == STRUCTURE_TOWER});
+        var towers = Game.spawns[spawn].room.find(FIND_STRUCTURES, {filter:
+          (structure) => structure.structureType == STRUCTURE_TOWER});
         for (var tower in towers){
             tower = towers[tower];
             var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
